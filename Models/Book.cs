5b63f81d464645d317db425a5044607c1790a438
hvictor186 <hvictorleite@gmail.com>
2022-08-15 15:11:15 -0300

    Corrige tipo propriedade Title

--- Models/Book.cs
+++ Models/Book.cs
@@ -4,7 +4,7 @@ namespace BookAPI.Models
     public class Book
     {
         public int Id { get; set; }
-        public int Title { get; set; }
+        public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
     }
